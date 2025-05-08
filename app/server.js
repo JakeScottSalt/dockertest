0089d77480b7728c421df87bac6d34b1ffbb9504
Jake <jakesalt@yahoo.com>
2025-05-08 04:16:01 +0100

    test

--- app/server.js
+++ app/server.js
@@ -14,11 +14,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-console.log("We're about to try and connect")
 // Database connection
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
-    .then(console.log("Hello world!"))
     .catch((err) => console.log(err));
 
 app.listen(process.env.PORT, () => {

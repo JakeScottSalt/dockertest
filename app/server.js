baa70512acf282fed7fff18fd444b07d9cf62efa
Jake <jakesalt@yahoo.com>
2025-05-08 03:51:31 +0100

    Added mongodb

--- app/server.js
+++ app/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
+
 const port = 3000;
 
 app.use(express.json());
@@ -9,6 +14,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+// Database connection
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log(err));
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
